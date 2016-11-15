6088516e8d07d7486e8ef5941c853b68b4dbc846
rubo77 <github@r.z11.de>
2016-11-15 14:31:17 +0100

    better warning to run in screen session and english comments
    mv /etc/hostname /var/tmp/hostname-bak befor overwrite

--- pre-puppet.sh
+++ pre-puppet.sh
@@ -15,7 +15,7 @@ echo "deb http://http.debian.net/debian wheezy-backports main" >>/etc/apt/source
 #sysupgrade
 apt-get update && apt-get upgrade && apt-get dist-upgrade
 
-#Benutzer hinzufügen
+#add users:
 
 #MOTD setzen
 rm /etc/motd
@@ -33,27 +33,28 @@ echo "**********************************************************" >>/etc/motd
 #Hostname setzen
 hostname "$HOST_PREFIX$VPN_NUMBER"
 echo "127.0.1.1 $SUBDOMAIN_PREFIX$VPN_NUMBER.$DOMAIN $HOST_PREFIX$VPN_NUMBER" >>/etc/hosts
+mv /etc/hostname /var/tmp/hostname-bak
 echo "$HOST_PREFIX$VPN_NUMBER" >/etc/hostname
 #benötigte Pakete installieren
 apt-get -y install sudo apt-transport-https bash-completion haveged git tcpdump mtr-tiny vim nano unp mlocate screen cmake build-essential libcap-dev pkg-config libgps-dev python3 ethtool lsb-release zip locales-all
 
-#REBOOT bei Kernel Panic
+#REBOOT on Kernel Panic
 echo "kernel.panic = 10" >>/etc/sysctl.conf
 
-#puppet Module installieren
-apt-get -y install --no-install-recommends puppet 
+#puppet modules install
+apt-get -y install --no-install-recommends puppet
 puppet module install puppetlabs-stdlib && puppet module install puppetlabs-apt --version 1.5.1 && puppet module install puppetlabs-vcsrepo --version 1.3.2 && puppet module install saz-sudo && puppet module install torrancew-account
 cd /etc/puppet/modules
 git clone https://github.com/ffnord/ffnord-puppet-gateway ffnord
 
-#check-services Script installieren
+#check-services script install
 cd /usr/local/bin
 wget --no-check-certificate https://raw.githubusercontent.com/Tarnatos/check-service/master/check-services
 chmod +x check-services
 chown root:root check-services
 sed 's/=ffnord/=fmdk/g' /usr/local/bin/check-services -i
 
-#zurück zu root
+# back in /root
 cd /root
 git clone https://github.com/Freifunk-Nord/nord-watchdog
 chmod +x /root/nord-watchdog/usr/local/bin/vpn-watchdog
@@ -64,7 +65,8 @@ cat > /etc/cron.d/vpn-watchdog <<EOF
 EOF
 
 #USER TODO:
-# copy manifest.pp, $keys, mesh_peerings.yaml to root 
-echo now copy the files manifest.pp, fastd_secret.key and mesh_peerings.yaml to /root
-echo and then start puppet apply --verbose /root/manifest.pp
-echo 'don´t run those scripts without screen sesssion!!!'
+echo 'now copy the files manifest.pp and mesh_peerings.yaml to /root and make sure /root/fastd_secret.key exists'
+echo '#########################################################################'
+echo '########### don´t run the script without screen sesssion!!! #############'
+echo '#########################################################################'
+echo 'start screen, then start puppet apply --verbose /root/manifest.pp'
